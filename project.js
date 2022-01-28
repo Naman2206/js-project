8d3a873317f7289f1dca12dbaa523b1c5a8be0ed
Naman2206 <namanmehta226@gmail.com>
2022-01-28 21:48:27 +0530

    add event lisner

--- project.js
+++ project.js
@@ -2,3 +2,8 @@ console.log("script is working")
 var username=prompt(username)
 var welcomemessage=("script works!!" +username)
 alert(welcomemessage)
+var btntranslate= document.querySelector("#btn-Translate");
+console.log(btntranslate)
+btntranslate.addEventListener("click",function clickeventhandler(){
+    console.log("clicked")
+} )
